b2a271676ea65488913b9febc4016ecacb51d981
Ms <qyg2297248353@163.com>
2021-07-23 00:18:15 +0800

    JSON 数据提交 数据获取 单个数据 多个数据

--- src/main/java/com/ms/controller/CategoryController.java
+++ src/main/java/com/ms/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package com.ms.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.ms.pojo.Category;
@@ -16,6 +20,38 @@ import com.ms.util.Page;
 @Controller
 @RequestMapping("")
 public class CategoryController {
+	@ResponseBody
+	@RequestMapping("submitCategory")
+	public String submitCategory(@RequestBody Category category) {
+		System.out.println("SSM接受浏览器提交的json，并转换对象："+category);
+		return "ok";
+	}
+	
+	@ResponseBody
+	@RequestMapping("getOneCategory")
+	public String getOneCategory() {
+		Category c=new Category();
+		c.setId(100);
+		c.setName("第一百个分类");
+		JSONObject json =new JSONObject();
+		json.put("category", JSONObject.toJSON(c));
+		return json.toJSONString();
+	}
+	
+	@ResponseBody
+	@RequestMapping("getManyCategory")
+	public String getManyCategory() {
+		List<Category> cs = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			Category c = new Category();
+			c.setId(i);
+			c.setName("分类名称："+i);
+			cs.add(c);
+		}
+		return JSONObject.toJSON(cs).toString();
+	}
+	
+	
 	@Autowired
 	CategoryService categoryService;
 	
